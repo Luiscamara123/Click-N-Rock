79d273444085fbee2e16b577bb4255b18104827e
Luís Câmara <luis.alcantaracamara@gmail.com>
2026-08-26 14:37:47 -0300

    update site

--- src/pages/meu-painel/meu-painel.php
+++ src/pages/meu-painel/meu-painel.php
@@ -10,13 +10,54 @@
 
 <body id="meu-painel">
 
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark px-3">
+      <a href="index.php"><img class="logo" src="./src/assets/img/icon/Logo.png"></a>
+      <input type="text" id="pesquisa" placeholder="Digite o produto">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"> </span>
+      </button>
+      <div id="navbarNav" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.php"> Início </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#servicos" id="servicosDropdown" role="button"
+              data-bs-toggle="dropdown"> Produtos </a>
+            <ul class="dropdown-menu">
+              <li> <a class="dropdown-item" href="#roupas"> Roupas </a></li>
+              <li> <a class="dropdown-item" href="#acessorios"> Acessórios </a></li>
+            </ul>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="#contato"> Ajuda </a></li>
+          <li class="nav-item"><a class="nav-link" href="#contato"> Contato </a></li>
+          <li class="nav-item"><a class="nav-link" href="./src/pages/meu-painel/meu-painel.php"> Meu painel </a></li>
+        </ul>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">
+          <img width="40px" src="./src/assets/img/icon/icon-usuario.png">
+        </button>
+        <div class="dropdown-content">
+          <a href="./src/pages/login/index.php">Login</a>
+          <a href="./src/pages/cadastrar/index.php">Cadastrar</a>
+        </div>
+      </div>
+      <div class="cart">
+        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png">
+        <div class="cart-count icon-carrinho" id="cartCount">0</div>
+      </div>
+    </nav>  
+  </header>
+
     <!-- DASHBOARD -->
     <div class="dashboard" id="dashboard">
 
         <!-- SIDEBAR -->
         <aside class="sidebar">
             <div class="logo">
-                <span>⚡</span> ROCK STORE
+                <span>⚡</span> CLICK N ROCK
             </div>
 
             <nav class="menu">
